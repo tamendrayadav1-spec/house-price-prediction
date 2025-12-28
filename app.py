0de586490f71c57b7b02cdd1ef25317a087f1d5c
tamendrayadav1-spec <tamendrayadav1@gmail.com>
2025-12-28 12:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ st.title('🏠House Price prediction using ML')
 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQdqIasHkDTf5733FK14z5mPQ18VPhg_R_Q&s')
 
-data = fetch_california_housing()
+df = pd.read_csv('house_data.csv')
+X = df.iloc[:,:-3]
+y = df.iloc[:,-1]
 
-X = pd.DataFrame(data['data'],
-            columns = data['feature_names'])
-
-final_X = X.iloc[:,:-2]
+final_X = X
 scaler = StandardScaler()
 scaled_X = scaler.fit_transform(final_X)
+
+st.sidebar.title('Select House features: ')
